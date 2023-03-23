e52c64ea8e3bc09b4c65209edbef3b98f25d1116
miteshSurti <114502371+miteshSurti@users.noreply.github.com>
2023-03-23 15:06:43 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: false,
+    env: {
+        BASE_URL: "https://finmanager-backend-production.up.railway.app",
+        WEB_URL: "http://localhost:3000",
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
